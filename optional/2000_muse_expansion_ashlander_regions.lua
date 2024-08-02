e6ca14599df491afce0d7bd6723327d4163e8397
eMOElein <aka_moe@gmx.de>
2024-08-02 20:28:55 +0200

    otional regions music

---
+++ optional/2000_muse_expansion_ashlander_regions.lua
@@ -0,0 +1,26 @@
+local soundBank = {
+  regionNames = {
+    'ashlands region',
+    'molag mar region'
+  },
+  tracks = {
+    {
+      path = 'Music/MS/cell/Ashlander/exploration1.mp3',
+      length = 231
+    },
+    {
+      path = 'Music/MS/cell/Ashlander/exploration2.mp3',
+      length = 252
+    },
+    {
+      path = 'Music/MS/cell/Ashlander/exploration3.mp3',
+      length = 308
+    },
+    {
+      path = 'Music/MS/cell/Ashlander/exploration4.mp3',
+      length = 562
+    }
+  }
+}
+
+return soundBank
